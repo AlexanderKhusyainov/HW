f3997489407f6ef5fa4edba48bb703e9231ef34b
«Alexander» <akhusyainov@gmail.com>
2022-10-16 13:05:13 +0300

    объявляю 2 массива

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+string[] array1 = new string[6] {"cat","no","1","feel","1984","good"};
+string[] array2 = new string[array1.Length];
