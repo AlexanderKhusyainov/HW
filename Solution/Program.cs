cc9c4cf02e0bf4307496663e51571c3b021da269
«Alexander» <akhusyainov@gmail.com>
2022-10-16 13:32:04 +0300

    задача решена

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -16,3 +16,24 @@ void SortingArray(string[] array1, string[]array2)
         }
     }
 }
+void PrintSortingArray(string[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(null != array[i])
+        {   
+            Console.Write($"{array[i]},");
+        }
+    }
+    Console.Write($"]");
+}
+
+Console.WriteLine($"Пользователь, программа формирует массив строк, длина которых меньше или равна 3 символа");
+Console.WriteLine();
+Console.Write($"Было ->");
+SortingArray(array1,array2);
+PrintSortingArray(array1);
+Console.Write($"  Стало ->");
+PrintSortingArray(array2);
+Console.WriteLine();
